d4f2f724fbda62d9392e285b46545b7d2afbac12
Arturo Hernandez <arturo.hernandez5@gmail.com>
2021-04-05 14:20:40 -0500

    fixed error when calculating with empty cost of service. Changed tip result to show 0 when calculating on empty cost after a previous calculation

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
         // .text returns an Editable, so toString() called first
-        val subtotal = binding.costOfService.text.toString().toDouble()
+        val subtotal = binding.costOfService.text.toString().toDoubleOrNull()
 
-        val tipId = binding.tipOptions.checkedRadioButtonId
+        // clear any previously entered results if empty calculation attempted
+        if (subtotal == null || subtotal == 0.0) {
+            displayTip(0.0)
+            return
+        }
 
-        val tipPercentage = when (tipId) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_fifteen_percent -> 0.15
             R.id.option_eighteen_percent -> 0.18
            else -> 0.20
@@ -32,11 +36,11 @@ class MainActivity : AppCompatActivity() {
         var tip = subtotal * tipPercentage
 
         if (binding.roundUpSwitch.isChecked) tip = kotlin.math.ceil(tip)
+        displayTip(tip)
+    }
 
+    fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
-
     }
 }
